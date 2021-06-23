2ce51ab4fafc8c29c7a8827267455608b95518d2
Salvador Sánchez <sanchezcampossalvador@gmail.com>
2021-06-23 11:31:52 +0200

    creado campo de 0 estrellas para la valoracion

--- src/componentes/FormularioAmigos.js
+++ src/componentes/FormularioAmigos.js
@@ -93,7 +93,7 @@ export const FormularioAmigos = (props) => {
               }}
               className="custom-select campo-form-amigo"
             >
-              <option></option>
+              <option value="0">0</option>
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
